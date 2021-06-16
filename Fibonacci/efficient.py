92f0eb1a81d1d4bd1daf66768fd7a82f6d815445
llxSKyWALKeRxll <79057173+llxSKyWALKeRxll@users.noreply.github.com>
2021-06-16 23:09:42 +0530

    Update efficient.py

--- Fibonacci/efficient.py
+++ Fibonacci/efficient.py
@@ -1,10 +1,7 @@
 fiboList = [0, 1]
 def fibonacci(n):
-    if n <= 1:
-        return n
-    else:
-        for i in range(2, n + 1):
-            fiboList.append(fiboList[i - 1] + fiboList[i - 2])
+    for i in range(2, n + 1):
+        fiboList.append(fiboList[i - 1] + fiboList[i - 2])
     return fiboList[n]
         
 
